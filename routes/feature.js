5f64a99571e248e7a0081048d321b9bb5ab4dc1e
saheelsapovadia <saheelsapovadia23@gmail.com>
2021-04-03 23:08:51 +0530

    Revert "Update feature.js"
    This reverts commit 6af2193bdd8ad361577471c5d676fe8e23ab40d5.

--- routes/feature.js
+++ routes/feature.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../models/user');
-const Novel = require('../models/novel');
+const Novel = require('../models/Novel');
 // @route   GET /:userid/bookmarklist
 // @desc    Get Bookmarked List
 // @access  Public
