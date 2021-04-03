c4fbaf68641227835b5f30482e189d074616e38a
saheelsapovadia <saheelsapovadia23@gmail.com>
2021-04-03 23:03:17 +0530

    Update feature.js

--- routes/feature.js
+++ routes/feature.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('../models/User');
+const User = require('../models/user');
 const Novel = require('../models/Novel');
 // @route   GET /:userid/bookmarklist
 // @desc    Get Bookmarked List
