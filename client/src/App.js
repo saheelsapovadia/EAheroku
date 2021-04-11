1fd97d30e86d66d26bfd330fc5d9d83c5ed40666
kakashi96 <68124692+kakashi96@users.noreply.github.com>
2021-04-11 19:37:54 +0530

    added images in public
    added new images in public folder in frontend

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import userReducer from './Store/reducers/userReducer';
-
 import thunk from 'redux-thunk';
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = 'G-C5GVGBFNY1'; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const rootReducer = combineReducers({ user: userReducer });
